6822163f8ab98cc886a3a0600ae99b3e0ec7c093
Scribbles-MCAMK <miles@mksystems.co>
2018-10-15 02:49:37 -0400

    Make number of inputs configurable in validatelargetx test

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2581,7 +2581,12 @@ UniValue zc_benchmark(const UniValue& params, bool fHelp)
         } else if (benchmarktype == "verifyequihash") {
             sample_times.push_back(benchmark_verify_equihash());
         } else if (benchmarktype == "validatelargetx") {
-            sample_times.push_back(benchmark_large_tx());
+            // Number of inputs in the spending transaction that we will simulate
+            int nInputs = 555;
+            if (params.size() >= 3) {
+                nInputs = params[2].get_int();
+            }
+            sample_times.push_back(benchmark_large_tx(nInputs));
         } else if (benchmarktype == "trydecryptnotes") {
             int nAddrs = params[2].get_int();
             sample_times.push_back(benchmark_try_decrypt_notes(nAddrs));
@@ -2843,7 +2848,7 @@ UniValue zc_raw_joinsplit(const UniValue& params, bool fHelp)
     CTransaction signTx(mtx);
     auto consensusBranchId = CurrentEpochBranchId(chainActive.Height() + 1, Params().GetConsensus());
     uint256 dataToBeSigned = SignatureHash(scriptCode, signTx, NOT_AN_INPUT, SIGHASH_ALL, 0, consensusBranchId);
-    
+
     // Add the signature
     assert(crypto_sign_detached(&mtx.joinSplitSig[0], NULL,
                          dataToBeSigned.begin(), 32,
